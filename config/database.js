f5d3de9aded520116b8cb87a27d5b08ff1c88c99
DinhNC <canhdinh2302@gmail.com>
2021-09-23 21:42:24 +0700

    Implement find blog by slug

--- config/database.js
+++ config/database.js
@@ -1,47 +1,22 @@
-const parse = require('pg-connection-string').parse
-const config = parse(process.env.DATABASE_URL)
-
-module.exports = ({ env }) => {
-  if (env('NODE_ENV') === 'production') {
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-            ssl: {
-              rejectUnauthorized: false,
-            },
-          },
-          options: {
-            ssl: true,
-          },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          rejectUnauthorized: false,
         },
       },
-    };
-  }
-
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'mysql',
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          username: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-        },
-        options: {},
+      options: {
+        ssl: true,
       },
     },
-  }
-}
+  },
+})
