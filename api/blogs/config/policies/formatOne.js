ce7e4f62fdeb5d934eff81ac123ff73678a6d722
DinhNC <canhdinh2302@gmail.com>
2021-09-30 22:34:48 +0700

    Implement for vote feature

--- api/blogs/config/policies/formatOne.js
+++ api/blogs/config/policies/formatOne.js
@@ -5,6 +5,11 @@ module.exports = async (ctx, next) => {
 
   const blog = {...ctx.body}
 
+  if (!blog.id) {
+    ctx.response.notFound()
+    return
+  }
+
   ctx.body = {
     ...blog,
     votes: formatVote(blog.votes, ctx.state.user)
--- api/blogs/controllers/blogs.js
+++ api/blogs/controllers/blogs.js
@@ -8,7 +8,41 @@ const { sanitizeEntity } = require('strapi-utils');
 module.exports = {
   async findOneSlug(ctx) {
     const { slug } = ctx.params;
-    const entity = await strapi.services.blogs.findOne({ slug });
-    return sanitizeEntity(entity, { model: strapi.models.blogs });
+    const blog = await strapi.services.blogs.findOne({ slug })
+    return sanitizeEntity(blog, { model: strapi.models.blogs })
+  },
+
+  async vote(ctx) {
+    const currentUser = ctx.state.user
+
+    if (!currentUser || !currentUser.id) {
+      ctx.response.unauthorized()
+      return
+    }
+
+    const { slug } = ctx.params
+    let blog = await strapi.services.blogs.findOne({ slug })
+
+    if (!blog || !blog.id) {
+      ctx.response.notFound()
+      return
+    }
+
+    let vote = await strapi.services.votes.findOne({ blog: blog.id, user: currentUser.id })
+    const point = ctx.request.body.point || 5
+
+    if (vote && vote.id) {
+      vote = await strapi.services.votes.update({ id: vote.id }, { point })
+    } else {
+      vote = await strapi.services.votes.create({
+        blog: blog.id,
+        user: currentUser.id,
+        point
+      })
+    }
+
+    blog = await strapi.services.blogs.findOne({ slug })
+
+    return sanitizeEntity(blog, { model: strapi.models.blogs })
   }
 }
