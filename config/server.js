b88b9d88f5ab1e819c1d1255dda085bfd7a29002
DinhNC <canhdinh2302@gmail.com>
2021-10-27 21:49:34 +0700

    Config to deploy heroku

--- config/server.js
+++ config/server.js
@@ -1,9 +1,18 @@
-module.exports = ({ env }) => ({
-  host: env('APP_HOST', '0.0.0.0'),
-  port: env.int('APP_PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+module.exports = ({ env }) => (
+  env('MY_HEROKU_URL') ? {
+    url: env('MY_HEROKU_URL'),
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET'),
+      },
     },
-  },
-})
+  } : {
+    host: env('APP_HOST', '0.0.0.0'),
+    port: env.int('APP_PORT', 1337),
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET'),
+      },
+    },
+  }
+)
